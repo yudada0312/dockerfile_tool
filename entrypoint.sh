926a3f576ef533a4e2a83ff2c586ea6b262914ed
yudada <yudada0312@gmail.com>
2019-10-14 16:53:31 +0800

    entrypoint.sh 增加變數「目錄名稱」

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+folder=/var/www/html/$1/public
+if [ ! -d "$folder" ]; then
+  echo "$folder not exist!"
+  return
+fi
 
-cd /var/www/html/public
+cd $folder
+
+chmod -R 777 storage/
+chmod -R 777 bootstrap/cache
+chmod 777 bootstrap/
 
 #取代 php artisan storage:link # 圖片上傳套件需要建立軟連結(相對路徑)
 ln -s ../storage/app/public storage
 echo “create soft link - image“
 
 #取代 php artisan theme:link # 為娛樂城佈景主題建立軟連結(相對路徑)
-for entry in /var/www/html/resources/views/casino/*
+for entry in /var/www/html/$1/resources/views/casino/*
 do
   dirname=$(basename "$entry")
   if [ $dirname = "__share__" ]
@@ -18,7 +27,6 @@ do
   echo “create soft link - ${dirname}“
 done
 
-cd /var/www/html
 #composer install
 
 # npm install
